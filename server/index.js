cb82ddd967aff0e42c0b48422fb2710763cf03e5
unknown <menukathandar@gmail.com>
2024-05-30 22:40:28 +0545

    Index file updated in server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/me', (req, res) => {
+//   res.send({
+//     name:"kaylin",
+//     balance: 100000,
+//     rewardPoint: 100
+//   })
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+// const express = require('express')
+// const app = express()
+// const port = 4000
+
+// app.get('/products', (req, res) => {
+//   res.send(['hawkins','baltra','panasonic'])
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+// server.mjs
+const{ createServer } = require('node:http');
+const server = createServer((req, res) => {
+  res.end('Hello World!\n');
+});
+server.listen(4000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});

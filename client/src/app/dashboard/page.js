f700ffdc26f3827c7cebb58a9fca05e04b7b9329
unknown <menukathandar@gmail.com>
2024-06-12 13:19:30 +0545

    NavBar Imported

--- client/src/app/dashboard/page.js
+++ client/src/app/dashboard/page.js
@@ -1,12 +1,17 @@
 'use client'
+
+import NavBar from "@/components/navbar/page";
+
+
+
 // import React from 'react'
 // import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react"
-import NavBar from '@/components/navbar/page'
+// import NavBar from '@/components/navbar/page'
 
 const dashboard= () => {
   return (
 <div>
-    <Navbar/>
+    <NavBar/>
  </div>
   );
 }
